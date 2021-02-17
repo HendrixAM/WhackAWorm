bf2b517ed88954c9c1efbd2917fbe935418c82c7
BuildTools <unconfigured@null.spigotmc.org>
2021-02-17 11:27:02 -0700

    Added onTouch Event

--- app/src/main/java/org/legendtitans/whackaworm/MainActivity.java
+++ app/src/main/java/org/legendtitans/whackaworm/MainActivity.java
@@ -3,6 +3,10 @@ package org.legendtitans.whackaworm;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageView imageViewPlay = (ImageView) findViewById(R.id.playImageView);
+        imageViewPlay.setOnTouchListener(new View.OnTouchListener() {
+
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                // TODO Auto-generated method stub
+                Log.i("imageviewandontouchlistener", "playImageView onTouch");
+                return false;
+            }
+        });
     }
 }
